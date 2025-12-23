2488242e0ba0a603f94e5fd51819ec27bebbcc85
ebatuhan <batuhane2401@gmail.com>
2025-12-23 20:02:34 +0300

    init

---
+++ plaid-adapter-service/src/main/java/com/batu/plaid_adapter_service/PlaidAdapterServiceApplication.java
@@ -0,0 +1,15 @@
+package com.batu.plaid_adapter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaidAdapterServiceApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PlaidAdapterServiceApplication.class, args);
+
+	}
+
+}
---
+++ plaid-adapter-service/src/test/java/com/batu/plaid_adapter_service/PlaidAdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batu.plaid_adapter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaidAdapterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
